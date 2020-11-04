a4dba3be0d7ca5af5e53da3c88eb85241246c7d7
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-04 22:33:13 +0530

    [Prudhvi] Add . maximum float value

---
+++ FindMaximumFloat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMaximum
+{
+    /// <summary>
+    /// checking for maximum value of input
+    /// </summary>
+    public class FindMaximumFloat
+    {
+        /// <summary>
+        /// returns the maxmimum Float value of given numbers
+        /// </summary>
+        /// <param name="firstValue"></param>
+        /// <param name="secondValue"></param>
+        /// <param name="thirdValue"></param>
+        /// <returns></returns>
+        public static float MaximumFloatNumber(float firstValue, float secondValue, float thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("all the numbers have same value");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace TestMaximum
             Console.WriteLine("Find Maximum value");
             int intOutput = FindMaximumInteger.MaximumIntegerNumber(10,100,1000);
             Console.WriteLine($"Maximum integer value is {intOutput}");
+            float floatOutput = FindMaximumFloat.MaximumFloatNumber(1.1f,2.1f,3.1f);
+            Console.WriteLine($"Maximum float value is {floatOutput}");
         }
     }
 }
