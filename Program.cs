c3735f9a977420d4c874c396737bc4cc3553bd89
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-04 22:44:45 +0530

    [Prudhvi] add .  find maximum string

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace TestMaximum
             Console.WriteLine($"Maximum integer value is {intOutput}");
             float floatOutput = FindMaximumFloat.MaximumFloatNumber(1.1f,2.1f,3.1f);
             Console.WriteLine($"Maximum float value is {floatOutput}");
+            string stringOutput = FindMaximumString.MaximumOfGivenStrings("Apple", "bananna", "Peach");
+            Console.WriteLine("maximum String is: " + stringOutput);
         }
     }
 }
