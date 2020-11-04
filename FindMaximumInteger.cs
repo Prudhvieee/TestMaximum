407ffdd51feba4293546b5b304942ca400fa7645
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-04 22:14:39 +0530

    [Prudhvi] Add . find the maximum integer

---
+++ FindMaximumInteger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMaximum
+{
+    /// <summary>
+    /// checking for maximum value of input
+    /// </summary>
+    public class FindMaximumInteger
+    {
+        /// <summary>
+        /// returns the maxmimum integer value of given numbers
+        /// </summary>
+        /// <param name="firstValue"></param>
+        /// <param name="secondValue"></param>
+        /// <param name="thirdValue"></param>
+        /// <returns></returns>
+        public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("all the numbers have same value");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace TestMaximum
         public static void Main(string[] args)
         {
             Console.WriteLine("Find Maximum value");
+            int intOutput = FindMaximumInteger.MaximumIntegerNumber(10,100,1000);
+            Console.WriteLine($"Maximum integer value is {intOutput}");
         }
     }
 }
