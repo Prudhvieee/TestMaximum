bc9c5b57c055b608c03f9008639531ad670015dd
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-06 08:29:58 +0530

    []Prudhvi] Add . adding generic class

--- Program.cs
+++ Program.cs
@@ -6,13 +6,12 @@ namespace TestMaximum
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Find Maximum value");
-            int intOutput = new FindMaximumValue.GenricMaximum<int>(10,100,1000).MaxMethod();
-            Console.WriteLine($"Maximum integer value is {intOutput}");
-            float floatOutput = new FindMaximumValue.GenricMaximum<float>(1.1f,2.1f,3.1f).MaxMethod();
-            Console.WriteLine($"Maximum float value is {floatOutput}");
-            string stringOutput = new FindMaximumValue.GenricMaximum<string>("Apple", "bananna", "Peach").MaxMethod();
-            Console.WriteLine("maximum String is: " + stringOutput);
+            int[] intArray = { 5, 8, 10, 44, 16 };
+            float[] floatArray = { 1.5f, 2.7f, 3.9f, 92.1f, 106.9f };
+            string[] stringArray = { "Bad", "CAT", "Polo", "BMW" };
+            new FindMaximumValue.GenericMaximum<int>(intArray).PrintMax();
+            new FindMaximumValue.GenericMaximum<float>(floatArray).PrintMax();
+            new FindMaximumValue.GenericMaximum<string>(stringArray).PrintMax();
         }
     }
 }
